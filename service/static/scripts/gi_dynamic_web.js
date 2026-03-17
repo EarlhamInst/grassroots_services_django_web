bf15e07fd52711f42d833e00e4f3bd05b99a68be
Fred Uche (EI) <kod25voz@n140804.comp.nbi.ac.uk>
2026-03-17 14:01:37 +0000

    undo referrerPolicy changes from master branch

--- service/static/scripts/gi_dynamic_web.js
+++ service/static/scripts/gi_dynamic_web.js
@@ -2441,7 +2441,7 @@ function display_result(json) {
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-            maxZoom: 18,referrerPolicy: 'strict-origin-when-cross-origin'
+            maxZoom: 18
         }).addTo(map);
 
         L.control.zoom({position: 'topright'}).addTo(map);
