c4c8df86b066555d6d4d9bcd609c7c34e7fedbb2
billyfish <tyrrell.simon@gmail.com>
2026-03-22 15:16:40 +0000

    adding tag for printing settings attributes

--- service/service_extras.py
+++ service/service_extras.py
@@ -1,3 +1,6 @@
+from django import template
+from django.conf import settings
+
 from django.template.defaulttags import register
 
 
@@ -5,3 +8,11 @@ from django.template.defaulttags import register
 def get_item(dictionary, key):
     return dictionary.get(key)
 
+
+
+register = template.Library()
+
+# settings value
+@register.simple_tag
+def settings_value(name):
+    return getattr(settings, name, "")
